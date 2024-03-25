1d428238988e678e7dd2209974228ad65170e5bd
yanxihang <964343812@qq.com>
2024-03-26 01:13:31 +0800

    1.MainUI主界面;2.同步属性UI及百分比;3.添加被动回复生命GA(Infinite-Duration-Modifier);4.构建技能信息结构体

---
+++ Source/GAS_Demo/Public/BaseGameplayAbility.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BaseGameplayAbility.generated.h"
+
+//技能消耗的类型
+UENUM()
+enum class ECostType : uint8
+{
+	HP,
+	MP,
+	Strength
+};
+
+USTRUCT(BlueprintType)
+struct FGameplayAbilityInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityInfo")
+	float CD;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityInfo")
+	ECostType CostType;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityInfo")
+	float CostValue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityInfo")
+	UMaterialInstance* IconMaterial;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityInfo")
+	TSubclassOf<UBaseGameplayAbility> AbilityClass;
+
+	FGameplayAbilityInfo(){}
+	FGameplayAbilityInfo(float CD, ECostType CostType, float CostValue, UMaterialInstance* MaterialInstance, TSubclassOf<UBaseGameplayAbility> AbilityClass);
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_DEMO_API UBaseGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityInfo")
+	UMaterialInstance* IconMaterial;
+
+	UFUNCTION(BlueprintCallable, Category = "AbilityInfo")
+	FGameplayAbilityInfo GetAbilityInfo(int level);
+};
